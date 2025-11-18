8c45f787b35a0382c4793d0a6212e4c726436563
Brock Rose <brock@MacBook-Air.local>
2025-11-18 16:50:21 -0600

    1ggle fix

--- app/signin/SigninForm.tsx
+++ app/signin/SigninForm.tsx
@@ -53,6 +53,7 @@ export default function SigninForm() {
           redirectTo: `${window.location.origin}/auth/callback?next=${encodeURIComponent(
             redirectTo
           )}`,
+          flowType: "pkce",
         },
       });
       if (error) {
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -61,6 +61,7 @@ export default function SignupPage() {
             prompt: "consent",
           },
           redirectTo: callbackUrl,
+          flowType: "pkce",
         },
       });
       if (error) {
