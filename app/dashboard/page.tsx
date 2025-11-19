5ecc06970c1dfacf7bc59038793ca3df6374d914
Brock Rose <brock@MacBook-Air.local>
2025-11-19 14:43:01 -0600

    new fix 3? unresolved google fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import DashboardClient from "./DashboardClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardPage() {
   const supabase = createServerComponentClient({ cookies });
   const {
